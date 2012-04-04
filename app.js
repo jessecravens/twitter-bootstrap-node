fb7362bba6d024132dbaf3873d864733674724ce
Jesse Cravens <jesse.cravens@gmail.com>
2012-04-03 23:09:58 -0500

    used html2jade to create example jade views, also added nav bar partial in jade

--- app.js
+++ app.js
@@ -8,59 +8,96 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
-// Configuration
-app.configure(function(){
-  app.use(express.static(__dirname + '/public'));
+// HTML Configuration
+	// app.configure(function(){
+	//   app.use(express.static(__dirname + '/public'));
+	// 
+	//   // disable layout
+	//   app.set("view options", {layout: false});
+	// 
+	//   // make a custom html template
+	//   app.register('.html', {
+	//     compile: function(str, options){
+	//       return function(locals){
+	//         return str;
+	//       };
+	//     }
+	//   });
+	// });
+	// 
+	// app.configure('development', function(){
+	//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	// });
+	// 
+	// app.configure('production', function(){
+	//   app.use(express.errorHandler());
+	// });
 
-  // disable layout
-  app.set("view options", {layout: false});
+// JADE Configuration ////////////////////////////////////////////////////////////////
+	app.configure(function(){
+	  app.set('views', __dirname + '/views');
+	  app.set('view engine', 'jade');
+	  app.use(express.bodyParser());
+	  app.use(express.methodOverride());
+	  // app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+	  app.use(app.router);
+	  app.use(express.static(__dirname + '/public'));
+	});
 
-  // make a custom html template
-  app.register('.html', {
-    compile: function(str, options){
-      return function(locals){
-        return str;
-      };
-    }
-  });
-});
+	app.configure('development', function(){
+	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	});
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+	app.configure('production', function(){
+	  app.use(express.errorHandler()); 
+	});
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+// Jade Routes ////////////////////////////////////////////////////////////////////////
+	
+	app.get('/', routes.index);
+	
+	// app.get('/', function(req, res){
+	//   res.render("index.jade");
+	// });
+	
+    app.get('/example1', routes.example1);
+	
+	// app.get('/example1', function(req, res){
+	//   res.render("example1.jade");
+	// });
+	
+	app.get('/example2', routes.example2);
+	app.get('/example3', routes.example3);
 
-// Routes
-app.get('/', function(req, res){
-  res.render("index.html");
-});
-app.get('/index.html', function(req, res){
-  res.render("index.html");
-});
-app.get('/scaffolding.html', function(req, res){
-  res.render("scaffolding.html");
-});
-app.get('/base-css.html', function(req, res){
-  res.render("base-css.html");
-});
-app.get('/components.html', function(req, res){
-  res.render("components.html");
-});
-app.get('/javascript.html', function(req, res){
-  res.render("javascript.html");
-});
-app.get('/less.html', function(req, res){
-  res.render("less.html");
-});
-app.get('/download.html', function(req, res){
-  res.render("download.html");
-});
-app.get('/examples.html', function(req, res){
-  res.render("examples.html");
-});
+
+// HTML Routes
+	// app.get('/', function(req, res){
+	//   res.render("index.html");
+	// });
+	// app.get('/index.html', function(req, res){
+	//   res.render("index.html");
+	// });
+	// app.get('/scaffolding.html', function(req, res){
+	//   res.render("scaffolding.html");
+	// });
+	// app.get('/base-css.html', function(req, res){
+	//   res.render("base-css.html");
+	// });
+	// app.get('/components.html', function(req, res){
+	//   res.render("components.html");
+	// });
+	// app.get('/javascript.html', function(req, res){
+	//   res.render("javascript.html");
+	// });
+	// app.get('/less.html', function(req, res){
+	//   res.render("less.html");
+	// });
+	// app.get('/download.html', function(req, res){
+	//   res.render("download.html");
+	// });
+	// app.get('/examples.html', function(req, res){
+	//   res.render("examples.html");
+	// });
 
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,17 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Index' })
+};
+
+exports.example1 = function(req, res){
+  res.render('example1', { title: 'Example1' })
+};
+
+exports.example2 = function(req, res){
+  res.render('example2', { title: 'Example2' })
+};
+
+exports.example3 = function(req, res){
+  res.render('example3', { title: 'Example3' })
 };
